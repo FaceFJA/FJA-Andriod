3cd210d4ce99a4d1c11f27fdbd2bc01354aecaed
eggbee <ty39563956@gmail.com>
2018-06-04 18:56:51 +0900

    make login,new_member,start activity

---
+++ app/src/main/java/com/example/ty395/fja/StartActivity.java
@@ -0,0 +1,35 @@
+package com.example.ty395.fja;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+    Button bt_new_member;
+    Button bt_login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        bt_new_member=(Button)findViewById(R.id.bt_new_member);
+        bt_login=(Button)findViewById(R.id.bt_login);
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        bt_new_member.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), MemberActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
