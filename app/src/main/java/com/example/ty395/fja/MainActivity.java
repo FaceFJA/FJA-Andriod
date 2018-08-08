7c8b4deace067b00594f6dd3d42188f9424330df
eggbee <ty39563956@gmail.com>
2018-08-08 14:20:09 +0900

    make floating action button(fab) Animation fab을 눌렀을때 글쓰기 버튼이 나오게 하는 애니메이션 구현완료

--- app/src/main/java/com/example/ty395/fja/MainActivity.java
+++ app/src/main/java/com/example/ty395/fja/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ty395.fja;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -7,19 +8,27 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Toolbar toolbar;
     Fragment1 fragment1;
     Fragment2 fragment2;
     Fragment3 fragment3;
     Fragment4 fragment4;
+    private Animation fab_open, fab_close;
+    private Boolean isFabOpen = false;
+    private FloatingActionButton fab, fab1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         //액션바 기본 타이틀 보여지지 않게
         ActionBar actionBar = getSupportActionBar();
@@ -30,43 +39,50 @@ public class MainActivity extends AppCompatActivity {
         fragment1 = new Fragment1();
         fragment2 = new Fragment2();
         fragment3 = new Fragment3();
-        fragment4=new Fragment4();
+        fragment4 = new Fragment4();
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab1.setOnClickListener(this);
+        fab.setOnClickListener(this);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
         //기본으로 첫번째 Fragment를 보여지도록 설정
-        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment1).commit();
 
 
         //TabLayout에 Tab 3개 추가
-        TabLayout tabs = (TabLayout)findViewById(R.id.tabs);
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
         tabs.addTab(tabs.newTab().setText("평가"));
         tabs.addTab(tabs.newTab().setText("토론"));
         tabs.addTab(tabs.newTab().setText("패셔니스타"));
         tabs.addTab(tabs.newTab().setText("마이페이지"));
 
         //탭 선택리스너
-        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             //탭선택시
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 int position = tab.getPosition();
 
                 Fragment selected = null;
-                if(position==0){
+                if (position == 0) {
                     selected = fragment1;
-                }else if(position==1){
+                } else if (position == 1) {
                     selected = fragment2;
-                }else if(position==2){
+                } else if (position == 2) {
                     selected = fragment3;
-                }
-                else if(position==3){
-                    selected=fragment4;
+                } else if (position == 3) {
+                    selected = fragment4;
                 }
                 getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
             }
+
             //탭선택해제시
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
 
             }
+
             //선탭된탭을 다시 클릭시
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
@@ -75,5 +91,32 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.fab:
+                anim();
+                Toast.makeText(this, "Floating Action Button", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.fab1:
+                anim();
+                Toast.makeText(this, "Button1", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    public void anim() {
+        if (isFabOpen) {
+            fab1.startAnimation(fab_close);
+            fab1.setClickable(false);
+            isFabOpen = false;
+        } else {
+            fab1.startAnimation(fab_open);
+            fab1.setClickable(true);
+            isFabOpen = true;
+        }
+    }
 
 }
