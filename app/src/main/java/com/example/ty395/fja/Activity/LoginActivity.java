9961e0cd1e73a707fc91113246032ee273c4ba9f
eggbee <ty39563956@gmail.com>
2019-01-16 18:00:20 +0900

    modified activity_login.xml(로그인 부분 레이아웃을 linear에서 constraint로 수정함)

--- app/src/main/java/com/example/ty395/fja/Activity/LoginActivity.java
+++ app/src/main/java/com/example/ty395/fja/Activity/LoginActivity.java
@@ -23,53 +23,57 @@ public class LoginActivity extends AppCompatActivity {
     EditText edit_password;
     String getEmail;
     String getPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        bt_login=(Button)findViewById(R.id.bt_login);
-        edit_email=(EditText)findViewById(R.id.edit_email);
-        edit_password=(EditText)findViewById(R.id.edit_password);
+        bt_login = (Button) findViewById(R.id.bt_login);
+        edit_email = (EditText) findViewById(R.id.edit_id);
+        edit_password = (EditText) findViewById(R.id.edit_pw);
         bt_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getEmail=edit_email.getText().toString();
-                getPassword=edit_password.getText().toString();
-                getEmail=getEmail.trim();
-                getPassword=getPassword.trim();
-                if(getEmail.getBytes().length<=0|getPassword.getBytes().length<=0){
-                    Toast.makeText(getApplicationContext(),"이메일 혹은 비밀번호가 입력되지 않았습니다",Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    post();
-                }
-            }
-        });
-    }
-    public void post(){
-        API retrofit= RetrofitService.getClient().create(API.class);
-        JsonObject jsonObject=new JsonObject();
-        jsonObject.addProperty("id",getEmail);
-        jsonObject.addProperty("pw",getPassword);
-        Call<Void> call= retrofit.post_login(jsonObject);
-        call.enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                Void repo = response.body();
-                if(response.code()==200) {
+                getEmail = edit_email.getText().toString();
+                getPassword = edit_password.getText().toString();
+                getEmail = getEmail.trim();
+                getPassword = getPassword.trim();
+                if (getEmail.getBytes().length <= 0 | getPassword.getBytes().length <= 0) {
+                    Toast.makeText(getApplicationContext(), "이메일 혹은 비밀번호가 입력되지 않았습니다", Toast.LENGTH_SHORT).show();
+                } else {
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
                     finish();
                 }
-                else{
-                    Toast.makeText(getApplicationContext(),"아이디 혹은 비밀번호가 맞지 않습니다.",Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-
             }
         });
     }
 }
+
+//    public void post() {
+//        API retrofit = RetrofitService.getClient().create(API.class);
+//        JsonObject jsonObject = new JsonObject();
+//        jsonObject.addProperty("id", getEmail);
+//        jsonObject.addProperty("pw", getPassword);
+//        Call<Void> call = retrofit.post_login(jsonObject);
+//        call.enqueue(new Callback<Void>() {
+//            @Override
+//            public void onResponse(Call<Void> call, Response<Void> response) {
+//                Void repo = response.body();
+////                if(response.code()==200) {
+////                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+////                    startActivity(intent);
+////                    finish();
+////                }
+////                else{
+////                    Toast.makeText(getApplicationContext(),"아이디 혹은 비밀번호가 맞지 않습니다.",Toast.LENGTH_SHORT).show();
+////                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Void> call, Throwable t) {
+//
+//            }
+//        });
+//    }
+//}
